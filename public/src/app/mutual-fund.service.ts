d6ea0859cb91f14a4134267501bdd83f2b1287bd
cindywgg <cindywan@andrew.cmu.edu>
2025-01-25 12:05:35 -0500

    Adding frontend

---
+++ public/src/app/mutual-fund.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface MutualFund {
+  ticker: string;
+  name: string;
+  description: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MutualFundService {
+  private apiUrl = 'http://localhost:8080/api/mutualfunds';
+
+  constructor(private http: HttpClient) { }
+
+  getMutualFunds(): Observable<MutualFund[]> {
+    return this.http.get<MutualFund[]>(this.apiUrl);
+  }
+}
